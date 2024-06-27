2e159fcc0554100aa4ff8e92e56d5bddf852e4a5
cuiruize <33984808+cuiruize@users.noreply.github.com>
2024-06-27 20:56:40 +0800

    Update prepare_dataset.py

--- utils/prepare_dataset.py
+++ utils/prepare_dataset.py
@@ -1,19 +1,20 @@
+import os
 from pathlib import Path
 
 
 def get_split(path):
-    train_path = Path(path)
+    dataset_path = Path(path)
 
     train_file_names = []
     val_file_names = []
     test_file_names = []
-    for instrument_id in range(1, 63):
-        if instrument_id in [21, 41, 31, 51]:
-            test_file_names += list((train_path / ('Patient_' + str(instrument_id)) / 'images').glob('*'))
-        elif instrument_id in [18, 40, 32]:
-            val_file_names += list((train_path / ('Patient_' + str(instrument_id)) / 'images').glob('*'))  # 122
+    sets = os.listdir(dataset_path)
+    for set in sets:
+        if set is "Train":
+            train_file_names = os.listdir(dataset_path / (set) / 'images')
+        elif set is "Test":
+            test_file_names = os.listdir(dataset_path / (set) / 'images')
         else:
-            train_file_names += list((train_path / ('Patient_' + str(instrument_id)) / 'images').glob('*'))  # 109
-
+            val_file_names = os.listdir(dataset_path / (set) / 'images')
 
     return train_file_names, test_file_names, val_file_names
